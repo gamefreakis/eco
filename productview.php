5f35038e0cfbc9b089994f41bde3bd9d0deaa3e5
exahzr <erzahaziri25@gmail.com>
2018-01-24 19:44:11 +0100

    Update productview.php

--- productview.php
+++ productview.php
@@ -42,10 +42,7 @@ while($row=mysqli_fetch_row($result))
     echo $row[0];
     echo ">".$row[1]."</a></li>";
 }
-
-    
-
-       
+   
        ?>
           
  
@@ -83,103 +80,53 @@ if(isset($_POST['view']))
    }
 }
 
-
-
-
-
 ?>
   
-<table border=1>
+<div class="container">
+  
+    	<div class="row">
+			<div class="col-md-10">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h3 class="panel-title">Product Preview</h3>
+						<div class="pull-right">
+							<span class="clickable filter" data-toggle="tooltip" title="Toggle table filter" data-container="body">
+								<i class="glyphicon glyphicon-filter"></i>
+							</span>
+						</div>
+					</div>
+					<div class="panel-body">
+						<input type="text" class="form-control" id="dev-table-filter" data-action="filter" data-filters="#dev-table" placeholder="Filter Developers" />
+					</div>
+					<table class="table table-hover" id="dev-table">
+						<thead>
+							<tr>
+								
+								<th>Title</th>
+								<th>Author</th>
+								<th>Publisher</th>
+								<th>Details</th>
+								<th>Price</th>
+								<th>Picture</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td><?php echo $name ?></td>
+								<td><?php echo $author ?></td>
+								<td><?php echo $publisher ?></td>
+								<td><?php echo $details ?></td>
+								<td><?php echo $price ?></td>
+								<td><?php  echo "<img src=images/";
+		echo $picture;
+		echo " />"; ?></td>
+							</tr>
+							
+						</tbody>
+					</table>
+                </div></div></div></div></div>
    
-    <tr>
-        <td>
-            Category: 
-        </td>
-        <td>
-            <?php echo $categoryid;?>
-        </td>
-    </tr>
-    <tr>
-        <td>
-           Title: 
-        </td>
-        <td>
-           <?php echo $name;?>
-        </td>
-    </tr>
-    <tr>
-        <td>
-           Author: 
-        </td>
-        <td>
-            <?php echo $author;?>
-        </td>
-    </tr>
-    <tr>
-        <td>
-           Publisher: 
-        </td>
-        <td>
-            <?php echo $publisher;?>
-        </td>
-    </tr>
-    <tr>
-        <td>
-           Details: 
-        </td>
-        <td>
-            <?php echo $details;?>
-        </td>
-    </tr>
-    <tr>
-        <td>
-           Price: 
-        </td>
-        <td>
-            <?php echo $price;?>
-        </td>
-    </tr>
-    <tr>
-        <td>
-           Picture: 
-        </td>
-        <td>
-            <?php echo $picture;?>
-        </td>
-    </tr>
-   
-	 </table>  
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-      </div>
-   
-   
-   
-   
-   
-   
-   
-   
-   
-
-         
-     
-    
-
+  
     <footer class="text-muted">
       <div class="container">
         <p class="float-right">
@@ -188,7 +135,7 @@ if(isset($_POST['view']))
         <p> &copy;SEEU, SKOPJE</p>
         <p>Want to know more? <a href="http://www.seeu.edu.mk">Visit the homepage</a>.</p>
       </div>
-    </footer>
+      </footer>
       </div>
     
   </body>
